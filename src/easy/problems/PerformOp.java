981dbfbf70d7244f278b1b2425b655c0e27546c5
Harshvijay Bundela <0harsh0000@gmail.com>
2022-08-04 09:42:40 +0530

    operations

---
+++ src/easy/problems/PerformOp.java
@@ -0,0 +1,18 @@
+package easy.problems;
+
+public class PerformOp {
+	class Solution {
+		public int finalValueAfterOperations(String[] operations) {
+			int x = 0;
+			for (int i = 0; i < operations.length; i++) {
+
+				if (operations[i].equals("--X") || operations[i].equals("X--")) {
+					--x;
+				} else {
+					++x;
+				}
+			}
+			return x;
+		}
+	}
+}
